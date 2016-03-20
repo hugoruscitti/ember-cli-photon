5cc6f4c7882ffc784c20c03de71564203d7e24bb
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-03-19 22:04:55 -0300

    update

--- blueprints/ember-cli-photon/index.js
+++ blueprints/ember-cli-photon/index.js
@@ -2,17 +2,6 @@ module.exports = {
   name: 'ember-cli-photon',
   normalizeEntityName: function() {},
 
-  included: function(app) {
-    this._super.included(app);
-    var path = {destDir: "fonts"};
-
-    this.app.import(app.bowerDirectory + '/photon/dist/css/photon.css');
-    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.eot', path);
-    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.svg', path);
-    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.ttf', path);
-    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.woff', path);
-  },
-
   blueprintsPath: function() {
     return path.join(__dirname, 'blueprints');
   },
--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-photon'
+  name: 'ember-cli-photon',
+
+  included: function(app) {
+    this._super.included(app);
+    var path = {destDir: "fonts"};
+
+    this.app.import(app.bowerDirectory + '/photon/dist/css/photon.css');
+    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.eot', path);
+    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.svg', path);
+    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.ttf', path);
+    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.woff', path);
+  },
 };
