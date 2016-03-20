ba7f098f0e2b05ae74f9d282974d6c45e49ff90c
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-03-19 21:55:08 -0300

    update

--- blueprints/ember-cli-photon/index.js
+++ blueprints/ember-cli-photon/index.js
@@ -6,11 +6,15 @@ module.exports = {
     this._super.included(app);
     var path = {destDir: "fonts"};
 
-    app.import(app.bowerDirectory + '/photon/dist/css/photon.css');
-    app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.eot', path);
-    app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.svg', path);
-    app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.ttf', path);
-    app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.woff', path);
+    this.app.import(app.bowerDirectory + '/photon/dist/css/photon.css');
+    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.eot', path);
+    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.svg', path);
+    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.ttf', path);
+    this.app.import(app.bowerDirectory + '/photon/dist/fonts/photon-entypo.woff', path);
+  },
+
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
   },
 
   afterInstall: function() {
